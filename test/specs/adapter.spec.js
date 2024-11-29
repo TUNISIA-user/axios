4bd9f647488eb630eab702b525878f8e22e7d871
Ghaith Nahdi <121437369+TUNISIA-user@users.noreply.github.com>
2024-11-29 14:57:36 +0100

    Update adapter.spec.js
    update code to aroow function ()=>  : )

--- test/specs/adapter.spec.js
+++ test/specs/adapter.spec.js
@@ -1,21 +1,24 @@
 
-describe('adapter', function () {
-  beforeEach(function () {
+describe('adapter',  ()=> {
+  beforeEach( () =>{
     jasmine.Ajax.install();
   });
 
-  afterEach(function () {
+
+
+  
+  afterEach( ()=> {
     jasmine.Ajax.uninstall();
   });
 
-  it('should support custom adapter', function (done) {
+  it('should support custom adapter',  (done)=> {
     axios('/foo', {
       adapter: function barAdapter(config) {
-        return new Promise(function dispatchXhrRequest(resolve) {
+        return new Promise( dispatchXhrRequest(resolve)=> {
           const request = new XMLHttpRequest();
           request.open('GET', '/bar');
 
-          request.onreadystatechange = function () {
+          request.onreadystatechange =  ()=> {
             resolve({
               config: config,
               request: request
@@ -27,21 +30,21 @@ describe('adapter', function () {
       }
     }).catch(done);
 
-    getAjaxRequest().then(function(request) {
+    getAjaxRequest().then((request) =>{
       expect(request.url).toBe('/bar');
       done();
     });
   });
 
-  it('should execute adapter code synchronously', function (done) {
+  it('should execute adapter code synchronously',  (done)=> {
     let asyncFlag = false;
     axios('/foo', {
       adapter: function barAdapter(config) {
-        return new Promise(function dispatchXhrRequest(resolve) {
+        return new Promise( dispatchXhrRequest(resolve) =>{
           const request = new XMLHttpRequest();
           request.open('GET', '/bar');
 
-          request.onreadystatechange = function () {
+          request.onreadystatechange =  () =>{
             resolve({
               config: config,
               request: request
@@ -56,26 +59,26 @@ describe('adapter', function () {
 
     asyncFlag = true;
 
-    getAjaxRequest().then(function() {
+    getAjaxRequest().then(()=> {
       done();
     });
   });
 
-  it('should execute adapter code asynchronously when interceptor is present', function (done) {
+  it('should execute adapter code asynchronously when interceptor is present',  (done) =>{
     let asyncFlag = false;
 
-    axios.interceptors.request.use(function (config) {
+    axios.interceptors.request.use( (config) =>{
       config.headers.async = 'async it!';
       return config;
     });
 
     axios('/foo', {
       adapter: function barAdapter(config) {
-        return new Promise(function dispatchXhrRequest(resolve) {
+        return new Promise( dispatchXhrRequest(resolve)=> {
           const request = new XMLHttpRequest();
           request.open('GET', '/bar');
 
-          request.onreadystatechange = function () {
+          request.onreadystatechange =  ()=> {
             resolve({
               config: config,
               request: request
@@ -90,7 +93,7 @@ describe('adapter', function () {
 
     asyncFlag = true;
 
-    getAjaxRequest().then(function() {
+    getAjaxRequest().then(() =>{
       done();
     });
   });
